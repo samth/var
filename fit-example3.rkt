2e3639840897eb235418d17c705c8fc2019b7b5f
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-06-06 00:32:53 -0700

    prep for fit demo

---
+++ fit-example3.rkt
@@ -0,0 +1,10 @@
+#lang s-exp "verified.rkt" ;trace
+
+(module prime? (any/c -> bool/c) ☁)
+;(module prime? (any/c -> bool/c) (λ z (if (nat? z) (= z 7) false)))
+(module keygen (any/c -> (pred prime?)) ☁) 
+;(module keygen (any/c -> (pred prime?)) (λ e 7)) 
+
+(module rsa ((any/c -> (pred prime?)) -> (string/c -> string/c)) ☁)
+((rsa keygen) "Plain")
+
