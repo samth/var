d03899fe1b59ccb94ccea09296f91202e207d227
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-07-13 00:35:15 -0400

    add insertion sort example

---
+++ sort.rkt
@@ -0,0 +1,14 @@
+#lang s-exp "verified.rkt"  
+
+(define-contract list/c
+  (rec/c X (or/c empty? (cons/c nat? X))))
+(module sorted-ne? (list/c -> bool?) ●)
+(module sorted? (list/c -> bool?) (λ (l) (if (empty? l) #t (sorted-ne? l))))
+(module insert (nat? (and/c list/c (pred sorted?)) -> (and/c list/c (pred sorted?))) ●)
+(module insertion-sort
+  (list/c (and/c list/c (pred sorted?)) -> (and/c list/c (pred sorted?)))
+  (λ (l acc) (if (empty? l) acc (insertion-sort (rest l) (insert (first l) acc)))))
+
+(module n list/c ●)
+(insertion-sort n empty)
+
