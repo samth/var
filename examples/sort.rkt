90cb2a3b17b050877365443cf830f7c09536294f
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-10-15 14:40:55 -0400

    more example work

--- examples/sort.rkt
+++ examples/sort.rkt
@@ -2,13 +2,30 @@
 
 (define-contract list/c
   (rec/c X (or/c empty? (cons/c nat? X))))
-(module sorted-ne? (list/c -> bool?) ●)
-(module sorted? (list/c -> bool?) (λ (l) (if (empty? l) #t (sorted-ne? l))))
-(module insert (nat? (and/c list/c sorted?) -> (and/c list/c sorted?)) ●)
-(module insertion-sort
-  (list/c (and/c list/c sorted?) -> (and/c list/c sorted?))
-  (λ (l acc) (if (empty? l) acc (insertion-sort (rest l) (insert (first l) acc)))))
 
-(module n list/c ●)
+(module sorted-ne? racket
+  ; ...
+  (provide/contract [sorted-ne? (list/c -> bool?)]))
+
+(module sorted? racket 
+  (require (only-in sorted-ne? sorted-ne?))
+  (define (sorted? l) (if (empty? l) #t (sorted-ne? l)))
+  (provide/contract [sorted? (list/c -> bool?)]))
+
+(module insert racket 
+  (require (only-in sorted?))
+  ; ...
+  (provide/contract [insert (nat? (and/c list/c sorted?) -> (and/c list/c sorted?))]))
+
+(module insertion-sort racket  
+  (require (only-in insert insert))
+  (define (insertion-sort l acc) (if (empty? l) acc (insertion-sort (rest l) (insert (first l) acc))))
+  (provide/contract [insertion-sort (list/c (and/c list/c sorted?) -> (and/c list/c sorted?))]))
+
+(module n racket 
+  ; ...
+  (provide/contract [n list/c]))
+
+(require (only-in n n) (only-in insertion-sort insertion-sort))
 (insertion-sort n empty)
 
