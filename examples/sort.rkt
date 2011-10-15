e67515dc275450429d61aa29e6ec8a25084d09eb
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-10-15 15:49:17 -0400

    Finally fixed sort.

--- examples/sort.rkt
+++ examples/sort.rkt
@@ -1,4 +1,4 @@
-#lang s-exp "../verified.rkt"  
+#lang s-exp "../verified.rkt"  ;count ;trace
 
 (define-contract list/c
   (rec/c X (or/c empty? (cons/c nat? X))))
@@ -13,12 +13,12 @@
   (provide/contract [sorted? (list/c -> bool?)]))
 
 (module insert racket 
-  (require (only-in sorted?))
+  (require (only-in sorted? sorted?))
   ; ...
   (provide/contract [insert (nat? (and/c list/c sorted?) -> (and/c list/c sorted?))]))
 
 (module insertion-sort racket  
-  (require (only-in insert insert))
+  (require (only-in insert insert) (only-in sorted? sorted?))
   (define (insertion-sort l acc) (if (empty? l) acc (insertion-sort (rest l) (insert (first l) acc))))
   (provide/contract [insertion-sort (list/c (and/c list/c sorted?) -> (and/c list/c sorted?))]))
 
