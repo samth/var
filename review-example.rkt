3d33dc309248e7965ecfc319167518dfa467d6e9
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-10-15 14:05:01 -0400

    new example

---
+++ review-example.rkt
@@ -0,0 +1,5 @@
+#lang s-exp "verified.rkt" ;trace
+(module foo racket (provide/contract [foo ((nat? -> nat?) -> nat?)]))
+(module addone racket (require) (define (addone n) (add1 n)) (provide/contract [addone (nat? -> nat?)]))
+(require (only-in foo foo) (only-in addone addone))
+(foo addone)
