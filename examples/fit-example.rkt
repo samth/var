ee9fd79f844ba64b1ba834179c2d8498a0be1496
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-10-16 16:57:33 -0400

    Use simple requires.

--- examples/fit-example.rkt
+++ examples/fit-example.rkt
@@ -6,7 +6,7 @@
 
 (module rsa racket (require (only-in prime? prime?)) (provide/contract [rsa (-> prime? (-> string? string?))]))
 
-(require (only-in rsa rsa) (only-in keygen keygen))
+(require rsa keygen)
 
 ((rsa (keygen #f)) "Plain")
 
