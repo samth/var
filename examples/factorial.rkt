ab993e4e37b55edcc5640a197f510ddc3dbd6a08
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-10-16 19:46:29 -0400

    more fact examples.

--- examples/factorial.rkt
+++ examples/factorial.rkt
@@ -1,11 +1,15 @@
-#lang var ;trace
+#lang var cesk-trace
 (module factorial racket 
+  (define (fact-acc n acc)
+    (if (zero? n) acc (fact-acc (sub1 n) (* n acc))))
+  (define (fact.1 n) (fact-acc n 1))
   (define (fact n) 
     (cond [(zero? n) 1]
           [else (* n (fact (sub1 n)))]))
   
   (provide/contract 
-   [fact (exact-nonnegative-integer? . -> . exact-nonnegative-integer?)]))
+   [fact.1 (exact-nonnegative-integer? . -> . exact-nonnegative-integer?)]
+   [fact   (exact-nonnegative-integer? . -> . exact-nonnegative-integer?)]))
     
 (require 'factorial)
-(fact 5)
+(fact 10)
