e573f04a42db586b9aebcd570bf92c74551468c0
David Van Horn <dvanhorn@ccs.neu.edu>
2011-10-16 01:04:08 -0400

    Partial cleanup to sort.rkt.

--- examples/sort.rkt
+++ examples/sort.rkt
@@ -17,7 +17,9 @@
   ; ...
   (provide/contract [insert (-> exact-nonnegative-integer? (and/c list/c sorted?) (and/c list/c sorted?))]))
 
-(module insertion-sort racket  
+(module insertion-sort racket 
+  ; BUG: doesn't work if written like this: 
+  ; (require 'insert 'sorted?)
   (require (only-in insert insert) (only-in sorted? sorted?))
   (define (insertion-sort l acc) (if (empty? l) acc (insertion-sort (rest l) (insert (first l) acc))))
   (provide/contract [insertion-sort (-> list/c (and/c list/c sorted?) (and/c list/c sorted?))]))
@@ -26,6 +28,6 @@
   ; ...
   (provide/contract [n list/c]))
 
-(require (only-in n n) (only-in insertion-sort insertion-sort))
+(require 'n 'insertion-sort)
 (insertion-sort n empty)
 
