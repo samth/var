d5494c6b333d580d41038b8ad34432e69b78fd3d
David Van Horn <dvanhorn@ccs.neu.edu>
2011-10-22 15:50:05 -0400

    Abstract module references.

---
+++ rho/step-m~.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(require redex/reduction-semantics)
+(require "lang.rkt" "meta.rkt" "util.rkt")
+(provide m~)
+(test-suite test step-m~)
+
+(define (m~ Ms)
+  (reduction-relation
+   λcρ #:domain D
+   (--> (X_1 ^ LAB X)
+        (CON () <= X LAB (-- (CON ())) X_1 
+             (remember-contract (-- (clos (λ (x) #t) ())) (CON ())))
+        (where • (lookup-modref/val X X_1 ,Ms))
+        (where CON (lookup-modref/con X X_1 ,Ms))
+        (side-condition (not (eq? (term X) (term LAB))))
+        m-opaque)))
+
+(test
+ (define Ms 
+   (term [(module m racket 
+            (require) 
+            (define f •)
+            (provide/contract [f (pred string? m)]))]))
+ 
+ (test--> (m~ Ms)
+          (term (f ^ † m))
+          (term ((pred string? m) 
+                 () <= m † (-- ((pred string? m) ())) f 
+                 (-- ((pred string? m) ()))))))
