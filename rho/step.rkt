76e59e0685960cbba2b3345f3e249dbea018d76f
David Van Horn <dvanhorn@ccs.neu.edu>
2011-10-21 20:06:03 -0400

    Blessed beta.

--- rho/step.rkt
+++ rho/step.rkt
@@ -225,13 +225,14 @@
          (@ (remember-contract V (C_arity ... -> (any/c))) (C_0 <= ℓ_2 ℓ_1 V_1 ℓ_3 V_1) ... Λ))
         (where (C_arity ...) ,(map (λ _ (term (any/c))) (term (C_0 ...))))
         blessed-β-dep)
-
-   (--> (@ ((C_0 ..._1 --> C_1) <= ℓ_1 ℓ_2 V-or-AE ℓ_3 V) V_1 ..._1 ℓ)        
-        (C_1 <= ℓ_1 ℓ_2 V-or-AE ℓ_3 
-             (@ (remember-contract V (C_arity ... -> (any/c))) (C_0 <= ℓ_2 ℓ_1 V_1 ℓ_3 V_1) ... Λ))
-        (where (C_arity ...) ,(map (λ _ (term (any/c))) (term (C_0 ...))))
-        blessed-β)
    |#
+   (--> (@ ((CON_0 ..._1 --> CON_1) ρ <= LAB_1 LAB_2 V_2 LAB_3 V) V_1 ..._1 LAB)        
+        (CON_1 ρ <= LAB_1 LAB_2 V_2 LAB_3 
+             (@ (remember-contract V ((CON_arity ... -> (pred (λ (x) #t) Λ)) ρ))
+                (CON_0 ρ <= LAB_2 LAB_1 V_1 LAB_3 V_1) ... Λ))
+        (where (CON_arity ...) ,(map (λ _ (term (pred (λ (x) #t) Λ))) (term (CON_0 ...))))
+        blessed-β)
+   
    ;; BLESSING
    (--> ((CON_1 ... -> any) ρ <= LAB_1 LAB_2 V_1 LAB_3 V)
         ((CON_1 ... --> any) ρ <= LAB_1 LAB_2 V_1 LAB_3
