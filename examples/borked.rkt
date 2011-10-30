3653ab9f4fdeb6f5800cb7f2bbedaff48f3aa1ff
David Van Horn <dvanhorn@ccs.neu.edu>
2011-10-29 21:54:10 -0400

    Borked that runs in #lang var rho trace.

--- examples/borked.rkt
+++ examples/borked.rkt
@@ -1,11 +1,11 @@
-#lang var cesk ;trace
+#lang var rho trace ;cesk ;trace
 
 
-(define/contract p (-> anything (-> anything boolean?)) (λ (x) (λ (y) x)))
+(define/contract p (-> any/c (-> any/c boolean?)) (λ (x) (λ (y) x)))
 
-(define/contract gen (-> anything (λ (z) (pred (λ (x) (z x))))) ☁)
+(define/contract gen (-> any/c (λ (z) (pred (λ (x) (z x))))) •)
 
-(define/contract gen2 (-> anything (λ (z1) (-> (pred (λ (x) (z1 x))) anything))) ☁)
+(define/contract gen2 (-> any/c (λ (z1) (-> (pred (λ (x) (z1 x))) any/c))) •)
 
 (require (only-in gen2 gen2) (only-in gen gen) (only-in p p))
 
