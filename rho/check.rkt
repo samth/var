5c4bfbbef1873b6f418e0ef3c2b7fe874672de7d
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-11-03 18:18:05 -0400

    use the expr simplifier. change handling of cons checks in flat-check to handle non-determinism.

--- rho/check.rkt
+++ rho/check.rkt
@@ -25,42 +25,48 @@
    (λ (X) #f)
    (where #t (contract-not-in (FLAT ρ) V))]  
   [(fc/c X any (and/c FLAT_1 FLAT_2) ρ V)
-   (λ (X) (if (@ EXP_1 X Λ) (@ EXP_2 X Λ) #f))
+   (λ (X) (IFF (@@ EXP_1 X Λ) (@@ EXP_2 X Λ) #f))
    (where EXP_1 (fc/c X any FLAT_1 ρ V))
    (where EXP_2 (fc/c X any FLAT_2 ρ V))]
   [(fc/c X any (not/c FLAT_1) ρ V)
-   (λ (X) (if (@ EXP X Λ) #f #t))
+   (λ (X) (IFF (@@ EXP X Λ) #f #t))
    (where EXP (fc/c X any FLAT_1 ρ V))]
   [(fc/c X any (cons/c FLAT_1 FLAT_2) ρ (-- (cons V_1 V_2) C ...))
-   (λ (X) (if (@ EXP_1 (@ car X Λ) Λ) (@ EXP_2 (@ cdr X Λ) Λ) #f))
+   (λ (X) (IFF (@@ EXP_1 (@ car X Λ) Λ) (@@ EXP_2 (@ cdr X Λ) Λ) #f))
    (where EXP_1 (fc/c X any FLAT_1 ρ V_1))
    (where EXP_2 (fc/c X any FLAT_2 ρ V_2))]
   [(fc/c X any (pred PREDV LAB) ρ V)
    ;; η-expansion for right blame label in case PREDV is not a predicate.
    (λ (X) (@ PREDV X LAB))]
   [(fc/c X any (or/c FLAT_1 FLAT_2) ρ V)
-   (λ (X) (if (@ EXP_1 X Λ) #t (@ EXP_2 X Λ)))
+   (λ (X) (IFF (@@ EXP_1 X Λ) #t (@@ EXP_2 X Λ)))
    (where EXP_1 (fc/c X any FLAT_1 ρ V))
    (where EXP_2 (fc/c X any FLAT_2 ρ V))]
   [(fc/c X_1 any (rec/c X CON) ρ V)   
    (fc/c X_1 ((((rec/c X CON) ρ) V) . any) (unroll (rec/c X CON)) ρ V)]
   [(fc/c X any (cons/c CON_1 CON_2) ρ AV)
-   (λ (X) (amb/e (@ EXP_n X Λ) ...))
+   (λ (X) (amb/e (@@ EXP_n X Λ) ...))
    (where (EXP_n ...)
           ,(remove-duplicates
             (for*/list ([l (term (proj-left AV))]
                         [r (term (proj-right AV))])
               (term (λ (x)
-                      (if (@ (fc/c X any CON_1 ρ ,l) (@ car x Λ) Λ)
-                          (@ (fc/c X any CON_2 ρ ,r) (@ cdr x Λ) Λ)
-                          #f))))))
+                      ;; this is neccessary because you might have gotten here through a proj-left/right
+                      ;; and therefore in the reduction semantics you don't know that this is actually a cons
+                      (IFF (@@ cons? x Λ) 
+                           (IFF (@@ (fc/c X any CON_1 ρ ,l) (@ car x Λ) Λ)
+                                (@@ (fc/c X any CON_2 ρ ,r) (@ cdr x Λ) Λ)
+                                #f)
+                           ;; final result is #t b/c this is a spurious result if we get here
+                           #t)))))) 
    (where #t (proves AV cons?))]
   [(fc/c X any (cons/c CON_1 CON_2) ρ AV)
-   (λ (X)
-     (amb/e #f
-            (if (@ EXP_1 • Λ)
-                (@ EXP_2 • Λ)
-                #f)))
+   (λ (x)
+     (IFF (@@ cons? x Λ)
+          (IFF (@@ EXP_1 • Λ)
+               (@@ EXP_2 • Λ)
+              #f)
+          #F))
    (where EXP_1 (fc/c X any CON_1 ρ (join-contracts)))
    (where EXP_2 (fc/c X any CON_2 ρ (join-contracts)))
                   
@@ -83,23 +89,23 @@
   (term (λ (x) (@ (prime? ^ f g) x f))))
  (test-equal
   (term (flat-check ((not/c (pred (prime? ^ f g) f)) (env)) (-- (clos 0 (env)))))
-  (term (λ (x) (if (@ (flat-check ((pred (prime? ^ f g) f) (env)) (-- (clos 0 (env)))) x Λ) #f #t))))        
+  (term (λ (x) (if (@@ (flat-check ((pred (prime? ^ f g) f) (env)) (-- (clos 0 (env)))) x Λ) #f #t))))        
  (test-equal
   (term (flat-check ((and/c (pred (prime? ^ f g) f)
                             (pred (composite? ^ f g) f)) 
                      (env)) 
                     (-- (clos 0 (env)))))
-  (term (λ (x) (if (@ (flat-check ((pred (prime? ^ f g) f) (env)) (-- (clos 0 (env)))) x Λ)
-                   (@ (flat-check ((pred (composite? ^ f g) f) (env)) (-- (clos 0 (env)))) x Λ) #f))))        
+  (term (λ (x) (if (@@ (flat-check ((pred (prime? ^ f g) f) (env)) (-- (clos 0 (env)))) x Λ)
+                   (@@ (flat-check ((pred (composite? ^ f g) f) (env)) (-- (clos 0 (env)))) x Λ) #f))))        
  (test-equal 
   (term (flat-check ((or/c (pred (prime? ^ f g) f)
                            (pred (composite? ^ f g) f)) 
                      (env)) 
                     (-- (clos 0 (env)))))
-  (term (λ (x) (if (@ (flat-check ((pred (prime? ^ f g) f) (env)) 
+  (term (λ (x) (if (@@ (flat-check ((pred (prime? ^ f g) f) (env)) 
                                   (-- (clos 0 (env)))) x Λ)
                    #t 
-                   (@ (flat-check ((pred (composite? ^ f g) f) (env)) 
+                   (@@ (flat-check ((pred (composite? ^ f g) f) (env)) 
                                   (-- (clos 0 (env)))) x Λ)))))
  (test-equal 
   (term (flat-check ((cons/c (pred (prime? ^ f g) f)
@@ -107,9 +113,9 @@
                      (env))
                     (-- (cons (-- (clos 0 (env))) (-- (clos 1 (env)))))))
   (term 
-   (λ (x) (if (@ (flat-check ((pred (prime? ^ f g) f) (env)) (-- (clos 0 (env))))
+   (λ (x) (if (@@ (flat-check ((pred (prime? ^ f g) f) (env)) (-- (clos 0 (env))))
                  (@ car x Λ) Λ)
-              (@ (flat-check ((pred (composite? ^ f g) f) (env)) (-- (clos 1 (env))))
+              (@@ (flat-check ((pred (composite? ^ f g) f) (env)) (-- (clos 1 (env))))
                  (@ cdr x Λ) Λ) #f))))
  (test-equal
   (term (flat-check ((rec/c z (pred (prime? ^ f g) f)) (env)) (-- (clos 0 (env)))))
@@ -131,8 +137,8 @@
                     (-- ((∧) (env)))))
   (term (λ (x1)
           (amb/e #f
-                 (if (@ (flat-check ((pred (prime? ^ f g) f) (env)) (join-contracts)) • Λ)
-                     (@ (flat-check ((pred (composite? ^ f g) f) (env)) (join-contracts)) • Λ)
+                 (if (@@ (flat-check ((pred (prime? ^ f g) f) (env)) (join-contracts)) • Λ)
+                     (@@ (flat-check ((pred (composite? ^ f g) f) (env)) (join-contracts)) • Λ)
                      #f)))))        
  (test-equal
   (term (flat-check ((rec/c x (or/c (pred empty? †)
@@ -140,9 +146,9 @@
                      (env))
                     (-- ((∧) (env)))))
   (term (λ (x1)
-          (if (@ (flat-check ((pred empty? †) (env)) (join-contracts)) x1 Λ)
+          (if (@@ (flat-check ((pred empty? †) (env)) (join-contracts)) x1 Λ)
               #t
-              (@ (λ (x1) (amb/e #f 
+              (@@ (λ (x1) (amb/e #f 
                                 (if (@ (flat-check ((pred zero? †) (env)) (join-contracts)) • Λ)
                                     (@ (λ (x1) #t) • Λ)
                                     #f)))
