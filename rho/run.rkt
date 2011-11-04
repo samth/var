f34d95abf2b15448c69e3430b0ce96a1e4d52aed
David Van Horn <dvanhorn@ccs.neu.edu>
2011-11-03 20:48:01 -0400

    Fixed up colorize.

--- rho/run.rkt
+++ rho/run.rkt
@@ -36,10 +36,10 @@
                     [(list 'module n lang (list 'define _ •) ... prov) n]
                     [_ #f]))
                 Ms))
-  (cond [(redex-match λcρ V x) "green"]
-        [(redex-match λcρ BLAME x)
+  (cond [(redex-match λcρ (V σ) x) "green"]
+        [(redex-match λcρ (BLAME σ) x)
          (redex-let λcρ
-                    ([(blame LAB any_0 V any_1 V_0) x])
+                    ([((blame LAB any_0 V any_1 V_0) σ) x])
                     (cond [(equal? (term LAB) '★) "pink"]
                           [(member (term LAB) opaques) "pink"]
                           [else "red"]))]
