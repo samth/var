35c8b8cf5e8bd81d874b9c912e3bfcc7bbc687a7
David Van Horn <dvanhorn@ccs.neu.edu>
2011-11-07 00:04:17 -0500

    Small presentation changes to dbl.

--- examples/dbl.rkt
+++ examples/dbl.rkt
@@ -1,19 +1,18 @@
-#lang var rho-trace
-(module e/o racket 
+#lang var rho eval
+(module e-o racket 
   (define even? (λ (n) (if (zero? n) #t (odd? (sub1 n)))))
-  (define odd?
-    (λ (n) (if (zero? n) #f (even? (sub1 n)))))
-  (provide/contract [even? (-> exact-nonnegative-integer? boolean?)]
-                    [odd? (-> exact-nonnegative-integer? boolean?)]))
+  (define odd?  (λ (n) (if (zero? n) #f (even? (sub1 n)))))
+  (provide/contract 
+   [even? (-> exact-nonnegative-integer? boolean?)]
+   [odd?  (-> exact-nonnegative-integer? boolean?)]))
 
 (module dbl racket
-  (require (only-in 'e/o even?))
+  (require 'e-o)
   (define dbl (λ (f) (λ (x) (f (f x)))))
   (provide/contract [dbl (-> (-> even? even?) (-> even? even?))]))
 
 (module fun racket
-  (require (only-in 'e/o even?))
-  ;...
+  (require 'e-o)
   (provide/contract [fun (-> even? even?)]))
 
 (require 'dbl 'fun)
