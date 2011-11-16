15ca3437bef8a2c8a37b62780f4452521f18ea54
David Van Horn <dvanhorn@ccs.neu.edu>
2011-11-16 18:40:18 -0500

    Sort for rho.

--- examples/sort.rkt
+++ examples/sort.rkt
@@ -1,27 +1,24 @@
-#lang var rho eval ;count ;trace
-(module sorted-ne? racket
-  ; ...
-  (provide/contract [sorted-ne? (-> (listof exact-nonnegative-integer?) boolean?)]))
+#lang var rho eval
 
-(module sorted? racket 
-  (require (only-in sorted-ne? sorted-ne?))
-  (define (sorted? l) (if (empty? l) #t (sorted-ne? l)))
+(module ne-sorted? racket
+  (provide/contract [ne-sorted? (-> (listof exact-nonnegative-integer?) boolean?)]))
+
+(module sorted? racket
+  (require 'ne-sorted?)
+  (define (sorted? x)
+    (if (empty? x) #t
+        (ne-sorted? x)))
   (provide/contract [sorted? (-> (listof exact-nonnegative-integer?) boolean?)]))
 
 (module insert racket 
-  (require (only-in sorted? sorted?))
+  (require 'sorted?)
   ; ...
   (provide/contract [insert (-> exact-nonnegative-integer? (and/c (listof exact-nonnegative-integer?) sorted?) (and/c (listof exact-nonnegative-integer?) sorted?))]))
 
 (module insertion-sort racket 
-  ; BUG: doesn't work if written like this: 
-  ; (require 'insert 'sorted?)
-  (require (only-in insert insert) (only-in sorted? sorted?))
+  (require 'insert 'sorted?)
   (define (insertion-sort l acc) (if (empty? l) acc (insertion-sort (cdr l) (insert (car l) acc))))
-  (provide/contract [insertion-sort ((listof exact-nonnegative-integer?) 
-                                     (and/c (listof exact-nonnegative-integer?) sorted?)
-                                     . -> .
-                                     (and/c (listof exact-nonnegative-integer?) sorted?))]))
+  (provide/contract [insertion-sort (-> (listof exact-nonnegative-integer?) (and/c (listof exact-nonnegative-integer?) sorted?) (and/c (listof exact-nonnegative-integer?) sorted?))]))
 
 (module n racket 
   ; ...
