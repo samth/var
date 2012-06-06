94dc70058358b006e05d2c6a33e550c562422e61
Phil Nguyen <pnguyen@ccs.neu.edu>
2012-06-06 19:12:15 -0400

    * support cons in language

--- cpcf/in-racket/scpcf-lang.rkt
+++ cpcf/in-racket/scpcf-lang.rkt
@@ -8,6 +8,7 @@
 (provide
  
  (contract-out
+  [struct ref ([distance natural?])]
   [struct value ([pre pre-value?] [refinements (set/c contract-clo?)])]
   [struct opaque ([type type?])]
   
@@ -19,20 +20,26 @@
                              [con contract/?] [exp exp?])]
   [struct prim-app ([op op?] [args (listof exp?)])]
   [struct blame/ ([violator label?] [violatee label?])]
-  [struct ref ([distance natural?])]
-  [struct mon-lam ([orig label?] [pos label?] [neg label?]
-                                 [con func/c?] [con-env env?]
-                                 [lam pre-value? #|can be opaque|#])]
+  [struct cons/ ([car exp?] [cdr exp?])]
+  [struct nil?/ ([of exp?])]
+  [struct cons?/ ([of exp?])]
+  [struct car/ ([of exp?])]
+  [struct cdr/ ([of exp?])]
   
   [struct flat/c ([exp exp?])]
   [struct func/c ([dom contract/?] [type type?] [rng contract/?])]
   
-  ;; hiding constructors 'clo' and 'contract-clo' would make it tedious
-  ;; due to lack of pattern matching. But client is expected to use
-  ;; 'close' instead of 'clo', and 'close-contract' instead of 'contract-clo'
-  [struct clo ([exp exp?] [env env?])]
-  [close (exp? env? . -> . clo?)]
+  ;; hiding closure constructors would make it tedious due to lack of
+  ;; pattern matching. But client is expected to use close instead of
+  ;; 'exp-clo', and 'close-contract' instead of 'contract-clo'
+  [struct exp-clo ([exp exp?] [env env?])]
+  [struct mon-fn-clo ([orig label?] [pos label?] [neg label?]
+                                    [con (struct/c contract-clo func/c? env?)]
+                                    [exp exp-clo?])]
+  [struct cons-clo ([car clo?] [cdr clo?])]
   [struct contract-clo ([con contract/?] [env env?])]
+  [clo? (any/c . -> . boolean?)]
+  [close (exp? env? . -> . clo?)]
   [close-contract (contract/? env? . -> . contract-clo?)]
   
   ;; Type := BaseType | FuncType | ConType
@@ -50,7 +57,6 @@
   
   [exp? (any/c . -> . boolean?)]
   [answer? (any/c . -> . boolean?)]
-  ;; PreValue := Opaque | Number | Boolean | Lambda
   [pre-value? (any/c . -> . boolean?)]
   [label? (any/c . -> . boolean?)]
   [op? (any/c . -> . boolean?)]
@@ -71,11 +77,20 @@
  c/any c/prime c/non-neg
  )
 
+;; ∅ : Setof x
+;; (eq? (set) (set)) = #f, and we have a lot of empty sets around,
+;; so I guess this might be useful. It looks nicer anyway.
+(define ∅ (set))
+
 ;; s-exp? : Any -> Boolean
 (define s-exp? any/c) ; TODO
 
 (struct exp () #:transparent)
 (struct answer exp () #:transparent)
+;; Value := (value PreValue [Listof ContractClosure])
+(struct value answer (pre refinements) #:transparent)
+;; Ref := (ref Natural)
+(struct ref exp (distance) #:transparent)
 ;; App := (app Exp Exp)
 (struct app exp (func arg) #:transparent)
 ;; Rec := (rec Type Exp)
@@ -86,29 +101,42 @@
 (struct prim-app exp (op args) #:transparent)
 ;; Mon := (mon Label Label label Contract Exp)
 (struct mon exp (orig pos neg con exp) #:transparent)
+;; Cons := (cons/ Exp Exp)
+(struct cons/ exp (car cdr) #:transparent)
 
-;; Value := (value PreValue [Listof ContractClosure])
-(struct value answer (pre refinements) #:transparent)
+;; Car := (car/ Exp)
+(struct car/ exp (of) #:transparent)
+;; Cdr := (cdr/ Exp)
+(struct cdr/ exp (of) #:transparent)
+;; Nil? := (nil?/ Exp)
+(struct nil?/ exp (of) #:transparent)
+;; Cons? := (cons?/ Exp)
+(struct cons?/ exp (of) #:transparent)
 
+;; Blame := (blame/ Label Label)
+(struct blame/ answer (violator violatee) #:transparent)
+
+;; PreValue := Opaque | Number | Boolean | Lambda
+(define (pre-value? x)
+  (or (number? x) (boolean? x) (opaque? x) (lam? x) (eq? 'nil x)))
 ;; Opaque := (opaque Type)
 (struct opaque (type) #:transparent)
 ;; Lambda := (lambda Type Exp)
 (struct lam (type body) #:transparent)
 ;; pre-value? : Any -> Boolean
-(define (pre-value? x)
-  (or (number? x) (boolean? x) (opaque? x) (lam? x) (mon-lam? x)))
 
-;; MonitoredLambda := (mon-lam Label Label Label FuncContract [Env Value] Exp)
-;; monitored lambda, for internal use only
-;; con-env is the environment that closes the monitoring contract, which is
-;; of the form (C1 ↦ λ.C2)
-(struct mon-lam (orig pos neg con con-env lam) #:transparent)
+;; Closure := ExpClosure | MonFnClosure | ConsClosure
+(struct clo () #:transparent)
+;; ExpClosure := (exp-clo Exp Env)
+(struct exp-clo clo (exp env) #:transparent)
+;; MonFnClosure := (mon-fn-clo Label Label Label ContractClosure Closure)
+(struct mon-fn-clo clo (orig pos neg con exp) #:transparent)
+;; ConsClosure := (cons-clo Closure Closure)
+(struct cons-clo clo (car cdr) #:transparent)
 
-;; Blame := (blame/ Label Label)
-(struct blame/ answer (violator violatee) #:transparent)
-
-;; Ref := (ref Natural)
-(struct ref exp (distance) #:transparent)
+;; close : Exp Env -> Closure
+(define (close exp en)
+  (clo exp (env-restrict (free-vars exp) en)))
 
 ;; Op := O1 | O2
 (define (op? o)
@@ -126,13 +154,6 @@
 (struct flat/c contract/ (exp) #:transparent)
 (struct func/c contract/ (dom type rng) #:transparent)
 
-;; Closure := (clo Exp [Env Value])
-(struct clo (exp env) #:transparent)
-
-;; close : Exp Env -> Closure
-(define (close exp en)
-  (clo exp (env-restrict (free-vars exp) en)))
-
 ;; ContractClosure = (contract-clo Contract [Env Value])
 (struct contract-clo (con env) #:transparent)
 
@@ -141,12 +162,12 @@
   (contract-clo con (env-restrict (con-free-vars con) en)))
 
 ;; type? : Any -> Boolean
-;; Type = BaseType | (func-type Type Type) | (con-type Type)
+;; Type = BaseType | (func-type Type Type) | (con-type Type) | (list-type type)
 (define (type? x)
-  (or (base-type? x) (func-type? x) (con-type? x)))
-
+  (or (base-type? x) (func-type? x) (con-type? x) (list-type? x)))
 (struct func-type (from to) #:transparent)
 (struct con-type (of) #:transparent)
+(struct list-type (of) #:transparent)
 
 ;; base-type? : Any -> Boolean
 ;; BaseType = 'Num | 'Bool | '⊥
@@ -156,9 +177,6 @@
 ;; TypeResult = Type | TypeError
 (define (type-result? x)
   (or (type? x) (equal? x 'TypeError)))
-
-;; ∅ : Set
-(define ∅ (set))
 
 ;; δ : Op [Listof Value] -> [Setof Answer]
 ;; applies primitive op
@@ -202,28 +220,35 @@
   (define (type-check-with tenv e)
     (match e
       [(ref d) (env-get-default d 'TypeError tenv)]
-      [(value (opaque t) refinements) t]
-      [(value (lam type body) refinements)
-       (extend func-type type (type-check-with (env-extend type tenv) body))]
+      [(value u cs) (match u
+                      [(opaque t) t]
+                      [(lam t b)
+                       (extend func-type t
+                               (type-check-with (env-extend t tenv) b))]
+                      ['nil (list-type '⊥)]
+                      [_ (cond
+                           [(number? u) 'Num]
+                           [(boolean? u) 'Bool])])]
       [(blame/ l1 l2) '⊥]
       [(app f x) (extend type-app
                          (type-check-with tenv f)
                          (type-check-with tenv x))]
-      [(rec type body) (type-check-with 
-                        (env-extend type tenv) body)]
+      [(rec t b) (type-check-with (env-extend t tenv) b)]
       [(if/ e1 e2 e3) (extend type-if
                               (type-check-with tenv e1)
                               (type-check-with tenv e2)
                               (type-check-with tenv e3))]
-      [(prim-app o args) (apply (curry extend (∆ o)) (map (curry type-check-with tenv) args))]
-      [(mon h f g c e)
-       (extend type-mon
-               (type-check-con-with tenv c)
-               (type-check-with tenv e))]
-      [(value u refinements)
-       (cond
-         [(number? u) 'Num]
-         [(boolean? u) 'Bool])]))
+      [(prim-app o xs) (apply (curry extend (∆ o)) (map (curry type-check-with tenv) xs))]
+      [(mon h f g c e) (extend type-mon
+                               (type-check-con-with tenv c)
+                               (type-check-with tenv e))]
+      [(cons/ e1 e2) (extend type-list
+                             (type-check-with tenv e1)
+                             (type-check-with tenv e2))]
+      [(nil?/ e) (extend (const 'Bool) (type-check-with tenv e))]
+      [(cons?/ e) (extend (const 'Bool) (type-check-with tenv e))]
+      [(car/ e) (extend type-car (type-check-with tenv e))]
+      [(cdr/ e) (extend type-cdr (type-check-with tenv e))]))
   
   ;; type-check-con-with : [Env TypeResult] Contract -> TypeResult
   (define (type-check-con-with tenv c)
@@ -249,13 +274,29 @@
   ;; type-app : Type Type -> TypeResult
   (define (type-app f arg)
     (match f
-      [(func-type dom rng) (if (equal? arg dom) rng 'TypeError)]
+      [(func-type dom rng) (if (⊑ arg dom) rng 'TypeError)]
       [else 'TypeError]))
   
   ;; type-if : Type Type Type -> TypeResult
   (define (type-if t1 t2 t3)
     (match t1
       ['Bool (⊔ t2 t3)]
+      [else 'TypeError]))
+  
+  ;; type-car : Type -> TypeResult
+  (define (type-car t)
+    (match t
+      [(list-type te) te]
+      [else 'TypeError]))
+  
+  ;; type-cdr : Type -> TypeResult
+  (define (type-cdr t)
+    (if (list-type? t) t 'TypeError))
+  
+  ;; type-list : Type Type -> TypeResult
+  (define (type-list te tl)
+    (match `(,te ,tl)
+      [`(,t ,(list-type t1)) (if (⊑ t1 t) (list-type t) 'TypeError)]
       [else 'TypeError]))
   
   ;; type-mon : Type Type -> TypeResult
@@ -274,6 +315,17 @@
       [`(,(func-type x y1) ,(func-type x y2)) (func-type x (⊔ y1 y2))]
       [`(,(con-type t1) ,(con-type t2)) (con-type (⊔ t1 t2))]
       [else 'TypeError]))
+  
+  ;; Type Type -> Boolean
+  (define (⊑ t1 t2)
+    (match `(,t1 ,t2)
+      [`(,t ,t) #t]
+      [`(⊥ ,t) #t]
+      [`(,(func-type tx1 ty1) ,(func-type tx2 ty2))
+       (and (⊑ tx2 tx1) (⊑ ty1 ty2))]
+      [`(,(list-type t3) ,(list-type t4)) (⊑ t3 t4)]
+      [`(,(con-type t3) ,(con-type t4)) (⊑ t3 t4)]
+      [_ #f]))
   
   ;; ∆ : Op -> ((Type -> TypeResult) or (Type Type -> TypeResult))
   (define (∆ o)
@@ -297,6 +349,7 @@
 ;; vars≥ : Exp -> [Setof Natural]
 (define (vars≥ d e)
   (match e
+    [(ref k) (if (>= k d) {set k} ∅)]
     [(value u cs) (match u
                     [(lam t b) (vars≥ (+ 1 d) b)]
                     [else ∅])]
@@ -306,8 +359,11 @@
     [(if/ e1 e2 e3) (set-union (vars≥ d e1) (vars≥ d e2) (vars≥ d e3))]
     [(prim-app o args) (apply set-union (map (curry vars≥ d) args))]
     [(mon h f g c e) (set-union (con-vars≥ d c) (vars≥ d e))]
-    [(mon-lam h f g c ρc e) (vars≥ d e)]
-    [(ref k) (if (>= k d) {set k} ∅)]))
+    [(cons/ e1 e2) (set-union (vars≥ d e1) (vars≥ d e2))]
+    [(nil?/ e) (vars≥ d e)]
+    [(cons?/ e) (vars≥ d e)]
+    [(car/ e) (vars≥ d e)]
+    [(cdr/ e) (vars≥ d e)]))
 ;; con-free-vars : Contract -> [Setof Natural]
 (define (con-free-vars c)
   (con-vars≥ 0 c))
@@ -332,6 +388,7 @@
     ['Bool 'Bool]
     ['⊥ '⊥] ; just for debugging
     [`(,t1 → ,t2) (func-type (read-type t1) (read-type t2))]
+    [`(List ,t) (list-type (read-type t))]
     [`(con ,t) (con-type (read-type t))]
     [else (error "invalid type form: " s)]))
 
@@ -382,16 +439,22 @@
      (if (andmap symbol? `(,h ,f ,g))
          (mon h f g (read-con-with names c) (read-exp-with names e))
          (error "mon: expect symbols, given: " h f g))]
+    [`(cons ,s1 ,s2) (cons/ (read-exp-with names s1)
+                            (read-exp-with names s2))]
+    [`(nil? ,s) (nil?/ (read-exp-with names s))]
+    [`(cons? ,s) (cons?/ (read-exp-with names s))]
+    [`(car ,s) (car/ (read-exp-with names s))]
+    [`(cdr ,s) (cdr/ (read-exp-with names s))]
     [`(,s0 ,s1)
      (if (o1? s0)
          (prim-app s0 (list (read-exp-with names s1)))
-         {app (read-exp-with names s0) (read-exp-with names s1)})]
+         (app (read-exp-with names s0) (read-exp-with names s1)))]
     [`(,s0 ,s1 ,s2)
      (if (o2? s0)
          (prim-app s0 (list (read-exp-with names s1) (read-exp-with names s2)))
          (error "expect binary op, given: " s0))]
     [x (cond
-         [(or (boolean? x) (number? x)) (value x ∅)]
+         [(pre-value? x) (value x ∅)]
          [(symbol? x) (ref (name-distance x names))]
          [else (error "invalid expression form: " x)])]))
 
@@ -411,11 +474,15 @@
   ;; show-exp-with : [Listof Symbol] -> S-exp
   (define (show-exp-with names e)
     (match e
-      [(value (opaque t) _) `(• ,(show-type t))]
-      [(value (lam t b) _)
-       (let ([x ((if (func-type? t) new-fun-name new-var-name) names)])
-         `(λ (,x ,(show-type t))
-            ,(show-exp-with (cons x names) b)))]
+      [(ref d) (list-ref names d)] ;; closed expressions can't cause error
+      [(value u _)
+       (match u
+         [(opaque t) `(• ,(show-type t))]
+         [(lam t b) 
+          (let ([x ((if (func-type? t) new-fun-name new-var-name) names)])
+            `(λ (,x ,(show-type t))
+               ,(show-exp-with (cons x names) b)))]
+         [c c])]
       [(blame/ l1 l2) `(blame l1 l2)]
       [(app f x) `(,(show-exp-with names f) ,(show-exp-with names x))]
       [(rec t b)
@@ -427,9 +494,12 @@
       [(mon h f g c e) `(mon ,h ,f ,g
                              ,(show-con-with names c)
                              ,(show-exp-with names e))]
-      [(value u refinements) u]
-      ;; closed expressions can't cause error
-      [(ref d) (list-ref names d)]))
+      [(cons/ e1 e2) `(cons ,(show-exp-with names e1)
+                            ,(show-exp-with names e2))]
+      [(nil?/ e) `(nil? ,(show-exp-with names e))]
+      [(cons?/ e) `(cons? ,(show-exp-with names e))]
+      [(car/ e) `(car ,(show-exp-with names e))]
+      [(cdr/ e) `(cdr ,(show-exp-with names e))]))
   
   ;; show-con-with : [Listof Symbol] Contract -> S-exp
   (define (show-con-with names c)
@@ -447,6 +517,7 @@
 (define (show-type t)
   (match t
     [(func-type dom rng) `(,(show-type dom) → ,(show-type rng))]
+    [(list-type t) `(List ,(show-type t))]
     [(con-type t) `(con ,(show-type t))]
     [t t]))
 
@@ -491,13 +562,17 @@
 (define rsa-ap
   `((,rsa (,keygen 13)) (• Num)))
 #;(define sqroot
-  `(mon h f g (,c/non-neg ↦ ,c/non-neg)
-        (λ (x Num) (sqrt x))))
+    `(mon h f g (,c/non-neg ↦ ,c/non-neg)
+          (λ (x Num) (sqrt x))))
 #;(define sqrt-user
-  `(mon h f g ((,c/any ↦ ,c/any) ↦ ,c/any)
-        (λ (f (Num → Num)) (,sqroot (f 0)))))
+    `(mon h f g ((,c/any ↦ ,c/any) ↦ ,c/any)
+          (λ (f (Num → Num)) (,sqroot (f 0)))))
 #;(define sqrt-ap
-  `(,sqrt-user (• (Num → Num))))
+    `(,sqrt-user (• (Num → Num))))
+(define sum
+  `(μ (f ((List Num) → Num))
+      (λ (xs (List Num))
+        (if (nil? xs) 0 (+ (car xs) (f (cdr xs)))))))
 
 ;;;;; testing
 (define exps (list ev? db1 db2 ap0 ap1 ap00 ap01 ap10 tri ap00-db2))
@@ -521,3 +596,4 @@
 #;(check-equal? (tc sqroot) '(Num → Num))
 #;(check-equal? (tc sqrt-user) '((Num → Num) → Num))
 #;(check-equal? (tc sqrt-ap) 'Num)
+(check-equal? (tc sum) '((List Num) → Num))
