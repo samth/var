a6f07bec61462fc645d6de02aa54db221ebd1f42
Phil Nguyen <pnguyen@ccs.neu.edu>
2013-01-13 15:36:47 -0500

    * restrict flat contract's syntax to value

--- cpcf/with-oc/lang-simple.rkt
+++ cpcf/with-oc/lang-simple.rkt
@@ -4,14 +4,13 @@
 (provide
  cpcf
  ⇓ ⇓c APP MON ev
- close ! :: subst subst/c FC AND OR)
+ close ! :: subst subst/c AND OR)
 
 (define-language cpcf
   [e a
      x
      (e e ...)
      (if e e e)
-     (μ x e)
      (mon c e)]
   [a v blame]
   [v (λ x e) b]
@@ -24,7 +23,7 @@
   [(m n) integer]
   [(x y z) variable-not-otherwise-mentioned]
   
-  [c (flat e) (c ↦ (λ x c)) (or/c c c) (and/c c c) (cons/c c c) (μ x c) x]
+  [c (flat v) (c ↦ (λ x c)) (or/c c c) (and/c c c) (cons/c c c) (μ x c) x]
   
   ; closed value
   [V b ((λ x e) ρ) (Arr (c ↦ (λ x c) ρ) V) (Cons V V)]
@@ -32,8 +31,7 @@
   [EA function b blame (Cons EA EA)]
   
   ; evaluated contract
-  [D (Flat V) (c ↦ (λ x c) ρ) (Or/c c c ρ) (And/c D D) (Cons/c D D)]
-  [Dns D blame]
+  [C V (c ↦ (λ x c) ρ) (Or/c C C) (And/c C C) (Cons/c c c ρ)]
   
   ; environment
   [ρ ([x ↦ V] ...)])
@@ -54,7 +52,8 @@
    (⇓ ρ e_1 A)
    ----- "if-true"
    (⇓ ρ (if e e_1 e_2) A)]
-  [(⇓ ρ e #f)
+  [(⇓ ρ e V)
+   (where #t (δ false? V))
    (⇓ ρ e_2 A)
    ----- "if-false"
    (⇓ ρ (if e e_1 e_2) A)]
@@ -71,18 +70,11 @@
    ----- "app-err"
    (⇓ ρ (e_1 ... e_i e_i+1 ...) blame)]
   
-  [(⇓ ρ [subst e x (μ x e)] A)
-   ----- "μ"
-   (⇓ ρ (μ x e) A)]
-  
-  [(⇓c ρ c D)
+  [(⇓c ρ c C)
    (⇓ ρ e V)
    ----- "mon"
-   (⇓ ρ (mon c e) [MON D V])]
-  [(⇓c ρ c blame)
-   ----- "mon-err-c"
-   (⇓ ρ (mon c e) blame)]
-  [(⇓c ρ c D)
+   (⇓ ρ (mon c e) [MON C V])]
+  [(⇓c ρ c C)
    (⇓ ρ e blame)
    ----- "mon-err-e"
    (⇓ ρ (mon c e) blame)])
@@ -90,47 +82,27 @@
 ;; contract evaluation
 (define-judgment-form cpcf
   #:mode     (⇓c I I O)
-  #:contract (⇓c ρ c Dns)
-  [(⇓ ρ e V)
-   ----- "flat"
-   (⇓c ρ (flat e) (Flat V))]
-  [(⇓ ρ e blame)
-   ----- "flat-err"
-   (⇓c ρ (flat e) blame)]
+  #:contract (⇓c ρ c C)
+  [----- "flat"
+   (⇓c ρ (flat v) [close v ρ])]
   
   [----- "func/c"
    (⇓c ρ (c_x ↦ (λ x c_y)) (c_x ↦ (λ x c_y) ρ))]
   
-  [----- "or/c"
-   (⇓c ρ (or/c c_1 c_2) (Or/c c_1 c_2 ρ))]
+  [(⇓c ρ c C) ...
+   ----- "or/c"
+   (⇓c ρ (or/c c ...) (Or/c C ...))]
   
-  [(⇓c ρ c_1 D_1)
-   (⇓c ρ c_2 D_2)
+  [(⇓c ρ c C) ...
    ----- "and/c"
-   (⇓c ρ (and/c c_1 c_2) (And/c D_1 D_2))]
-  [(⇓c ρ c_1 blame)
-   ----- "and/c-err-1"
-   (⇓c ρ (and/c c_1 c_2) blame)]
-  [(⇓c ρ c_1 D_1)
-   (⇓c ρ c_2 blame)
-   ----- "and/c-err-2"
-   (⇓c ρ (and/c c_1 c_2) blame)]
+   (⇓c ρ (and/c c ...) (And/c C ...))]
   
-  [(⇓c ρ c_1 D_1)
-   (⇓c ρ c_2 D_2)
-   ----- "cons/c"
-   (⇓c ρ (cons/c c_1 c_2) (Cons/c D_1 D_2))]
-  [(⇓c ρ c_1 blame)
-   ----- "cons/c-err-1"
-   (⇓c ρ (cons/c c_1 c_2) blame)]
-  [(⇓c ρ c_1 D_1)
-   (⇓c ρ c_2 blame)
-   ----- "cons/c-err-2"
-   (⇓c ρ (cons/c c_1 c_2) blame)]
+  [----- "cons/c"
+   (⇓c ρ (cons/c c_1 c_2) (Cons/c c_1 c_2 ρ))]
   
-  [(⇓c ρ [subst/c c x (μ x c)] Dns)
+  [(⇓c ρ [subst/c c x (μ x c)] C)
    ---- "μ/c"
-   (⇓c ρ (μ x c) Dns)])
+   (⇓c ρ (μ x c) C)])
   
 ;; application
 (define-metafunction cpcf
@@ -140,44 +112,75 @@
    (where (A) ,(judgment-holds (⇓ [:: ρ (x ↦ V_x)] e_y A) A))]
   [(APP (Arr (c_x ↦ (λ x c_y) ρ) V_f) V_x)
    A
-   (where (D_x) ,(judgment-holds (⇓c ρ c_x D) D))
-   (where V_x′ (MON D_x V_x))
+   (where (C_x) ,(judgment-holds (⇓c ρ c_x C) C))
+   (where V_x′ (MON C_x V_x))
    (where V_y (APP V_f V_x′))
-   (where (D_y) ,(judgment-holds (⇓c [:: ρ [x ↦ V_x′]] c_y D) D))
-   (where A (MON D_y V_y))]
+   (where (C_y) ,(judgment-holds (⇓c [:: ρ [x ↦ V_x′]] c_y C) C))
+   (where A (MON C_y V_y))]
+  [(APP (Arr (c_x ↦ (λ x c_y) ρ) V_f) V_x)
+   blame
+   (where (C_x) ,(judgment-holds (⇓c ρ c_x C) C))
+   (where blame (MON C_x V_x))]
+  [(APP (Arr (c_x ↦ (λ x c_y) ρ) V_f) V_x)
+   blame
+   (where (C_x) ,(judgment-holds (⇓c ρ c_x C) C))
+   (where V_x′ (MON C_x V_x))
+   (where blame (APP V_f V_x′))]
   [(APP op V_x ...) (δ op V_x ...)])
 
 ;; monitoring
 (define-metafunction cpcf
-  MON : D A -> A
+  MON : C A -> A
   ; flat
-  [(MON (Flat V_p) V) blame (where #f (APP V_p V))]
-  [(MON (Flat V_p) V) blame (where blame (APP V_p V))]
-  [(MON (Flat V_p) V) V]
+  [(MON V_p V) blame (where #f (APP V_p V))]
+  [(MON V_p V) blame (where blame (APP V_p V))]
+  [(MON V_p V) V]
   ; func
   [(MON (c_x ↦ (λ x c_y) ρ) V) blame (where #f (δ proc? V))]
   [(MON (c_x ↦ (λ x c_y) ρ) V) (Arr (c_x ↦ (λ x c_y) ρ) V)]
   ; or/c
-  [(MON (Or/c c_1 c_2 ρ) V)
+  [(MON (Or/c C_1 C_2) V)
    V
-   (where (V_p) ,(judgment-holds (⇓ ρ (FC c_1) V) V))
-   (where V_t (APP V_p V))
+   (where V_t (FC C_1 V))
    (where #f (δ false? V_t))]
-  [(MON (Or/c c_1 c_2 ρ) V)
-   (MON D_2 V)
-   (where (V_p) ,(judgment-holds (⇓ ρ (FC c_1) V) V))
-   (where #f (APP V_p V))
-   (where (D_2) ,(judgment-holds (⇓c ρ c_2 D) D))]
+  [(MON (Or/c C_1 C_2) V) (MON C_2 V) (where #f (FC C_1 V))]
+  [(MON (Or/c C_1 C_2) V) blame (where blame (FC C_1 V))]
   ; and/c
-  [(MON (And/c D_1 D_2) V) (MON D_2 (MON D_1 V))]
+  [(MON (And/c C_1 C_2) V) (MON C_2 (MON C_1 V))]
   ; cons/c
-  [(MON (Cons/c D_1 D_2) (Cons V_1 V_2))
+  [(MON (Cons/c c_1 c_2 ρ) (Cons V_1 V_2))
    (Cons V_1′ V_2′)
-   (where V_1′ (MON D_1 V_1))
-   (where V_2′ (MON D_2 V_2))]
-  [(MON (Cons c_1 c_2 ρ) V) blame]
+   (where (C_1) ,(judgment-holds (⇓c ρ c_1 C) C))
+   (where V_1′ (MON C_1 V_1))
+   (where (C_2) ,(judgment-holds (⇓c ρ c_2 C) C))
+   (where V_2′ (MON C_2 V_2))]
+  [(MON (Cons/c c_1 c_2 ρ) V) blame]
   ; propagate blame
-  [(MON D blame) blame])
+  [(MON C blame) blame])
+(define-metafunction cpcf
+  FC : C V -> V
+  [(FC V_p V) (APP V_p V)]
+  [(FC (Or/c C_1 C_2) V) (OR (FC C_1 V) (FC C_2 V))]
+  [(FC (And/c C_1 C_2) V) (AND (FC C_1 V) (FC C_2 V))]
+  [(FC (Cons/c c_1 c_2 ρ) (Cons V_1 V_2))
+   (AND (FC C_1 V_1) (FC C_2 V_2))
+   (where (C_1) ,(judgment-holds (⇓c ρ c_1 C) C))
+   (where (C_2) ,(judgment-holds (⇓c ρ c_2 C) C))]
+  [(FC (Cons/c c_1 c_2 ρ) V) #f])
+(define-metafunction cpcf
+  OR : A ... -> A
+  [(OR) #f]
+  [(OR A) A]
+  [(OR #f A ...) (OR A ...)]
+  [(OR V A ...) V])
+(define-metafunction cpcf
+  AND : A ... -> A
+  [(AND) #t]
+  [(AND A) A]
+  [(AND #f A ...) #f]
+  [(AND blame A ...) blame]
+  [(AND V A ...) (AND A ...)])
+
 
 ;; close value
 (define-metafunction cpcf
@@ -223,44 +226,6 @@
   [(subst/c x z any) x]
   [(subst/c (any_l c ...) x any) (any_l (subst/c c x any) ...)])
 
-;; flattens contract
-(define-metafunction cpcf
-  FC : c -> e
-  [(FC (flat e)) e]
-  [(FC (or/c c_1 c_2))
-   (λ x (OR (e_1 x) (e_2 x)))
-   (where e_1 (FC c_1))
-   (where e_2 (FC c_2))
-   (where x ,(variable-not-in (term (e_1 e_2)) (term X)))]
-  [(FC (and/c c_1 c_2))
-   (λ x (AND (e_1 x) (e_2 x)))
-   (where e_1 (FC c_1))
-   (where e_2 (FC c_2))
-   (where x ,(variable-not-in (term (e_1 e_2)) (term X)))]
-  [(FC (cons/c c_1 c_2))
-   (λ x (AND/c (cons? x) (e_1 x) (e_2 x)))
-   (where e_1 (FC c_1))
-   (where e_2 (FC c_2))
-   (where x ,(variable-not-in (term (e_1 e_2)) (term X)))]
-  [(FC (μ x c)) (μ x (FC c))]
-  [(FC x) x]
-  [(FC (c_x ↦ (λ x c_y))) ,(error "flat!")])
-
-;; macros
-(define-metafunction cpcf
-  AND : e ... -> e
-  [(AND) #t]
-  [(AND e) e]
-  [(AND e_1 e ...) (if e_1 (AND e ...) #f)])
-(define-metafunction cpcf
-  OR : e ... -> e
-  [(OR) #f]
-  [(OR e) e]
-  [(OR e_1 e ...)
-   ((λ x (if x x e_rest)) e_1)
-   (where e_rest (OR e ...))
-   (where x ,(variable-not-in (term e_rest) (term tmp)))])
-
 ;; primitive ops
 (define-metafunction cpcf
   δ : op V ... -> A
@@ -270,7 +235,7 @@
   [(δ false? V) #f]
   [(δ proc? op) #t]
   [(δ proc? ((λ x e) ρ)) #t]
-  [(δ proc? (Arr D V)) #t]
+  [(δ proc? (Arr C V)) #t]
   [(δ proc? V) #f]
   [(δ cons? (Cons V_1 V_2)) #t]
   [(δ cons? V) #f]
@@ -311,4 +276,7 @@
 (test-equal (term (ev (mon ((flat int?) ↦ (λ z (flat int?))) (λ x x)))) 'function)
 (test-equal (term (ev ((mon ((flat int?) ↦ (λ z (flat int?))) (λ x x)) 1))) 1)
 (test-equal (term (ev ((mon ((flat int?) ↦ (λ z (flat false?))) (λ x x)) 1))) 'blame)
+(test-equal (term (ev (mon (μ list? (or/c (flat false?) (cons/c (flat int?) list?)))
+                            (cons 1 (cons 2 #f)))))
+            `(Cons 1 (Cons 2 #f)))
 (test-results)
